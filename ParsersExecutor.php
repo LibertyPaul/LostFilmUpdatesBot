f5b342b91ce6613ac79cf63d8b0ef8fbb2bcada4
LibertyPaul <admin@libertypaul.ru>
2016-05-10 16:05:02 +0100

    ShowOnAirStateUpdater was moved to ParsersExecutor

--- ParsersExecutor.php
+++ ParsersExecutor.php
@@ -2,6 +2,7 @@
 require_once(realpath(dirname(__FILE__))."/config/stuff.php");
 require_once(realpath(dirname(__FILE__))."/ShowParser.php");
 require_once(realpath(dirname(__FILE__))."/SeriesParser.php");
+require_once(realpath(dirname(__FILE__)).'/ShowAboutParser.php');
 require_once(realpath(dirname(__FILE__))."/config/cron_actions.php");
 
 
@@ -40,6 +41,32 @@ $seriesParser->loadSrc(rssURL);
 $seriesParser->run();
 
 
+/*
+$sap = new ShowAboutParser('CP1251');
+$pdo = createPDO();
 
+$getShow = $pdo->query("
+	SELECT `id`, `title_ru`
+	FROM `shows`
+");
 
+$updateShow = $pdo->prepare("
+	UPDATE `shows`
+	SET `onAir` = :onAir
+	WHERE `id` = :id
+");
+
+while($show = $getShow->fetchObject()){
+	$sap->loadSrc("https://www.lostfilm.tv/browse.php?cat={$show->url_id}");
+	$res = $sap->run();
+	$updateShow->execute(
+		array(
+			':onAir' => $res,
+			':id' => $show->id
+		)
+	);
+	
+	echo "{$show->id} : $res\n";
+}
+*/
 
