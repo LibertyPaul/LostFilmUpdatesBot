c4b01ea784a801b7bca8ac1ce4b913e5ab78880d
LibertyPaul <admin@libertypaul.ru>
2016-11-05 16:20:05 +0000

    Message mocking logic now moved to MessageTester class

---
+++ tests/MessageTester.php
@@ -0,0 +1,128 @@
+<?php
+
+require_once(__DIR__.'/TelegramBotMockFactory.php');
+require_once(__DIR__.'/../Exceptions/TelegramException.php');
+
+class MessageTester{
+
+	const messageTemplate = '{
+		"update_id": 0,
+		"message": {
+			"message_id": 0,
+			"from": {
+			    "id": #TELEGRAM_ID,
+			    "first_name": "#FIRST_NAME",
+			    "username": "#USERNAME"
+			},
+			"chat": {
+			    "id": #TELEGRAM_ID,
+			    "first_name": "#FIRST_NAME",
+			    "username": "#USERNAME",
+			    "type": "private"
+			},
+			"date": 0,
+			"text": "#TEXT"
+		}
+	}';
+
+	const filedsToInsert = array(
+		array('key' => '#TEXT', 		'defaultValue' => null),
+		array('key' => '#FIRST_NAME', 	'defaultValue' => 'test first name'),
+		array('key' => '#USERNAME', 	'defaultValue' => 'test username')
+	);
+	
+	private $telegram_id;
+	private $bot;
+	private $botOutputFile;
+
+	public function __construct($telegram_id){
+		assert(is_int($telegram_id));
+		$this->telegram_id = $telegram_id;
+		
+		$this->botOutputFile = tempnam(sys_get_temp_dir(), 'MessageTester_');
+
+		$factory = new TelegramBotMockFactory($this->botOutputFile);
+
+		$this->bot = $factory->createBot($this->telegram_id);
+	}
+	
+	private function fillTemplate(array $fields){
+		$keys = array();
+		$values = array();
+		
+		foreach(self::filedsToInsert as $fieldToInsert){
+			$key = $fieldToInsert['key'];
+			$keys[] = $key;
+			$value = null;
+			
+			if(isset($fields[$key])){
+				$value = $fields[$key];
+			}
+			else{
+				if($fieldToInsert['defaultValue'] === null){
+					throw new Exception($fieldToInsert['key'].' is required');
+				}
+				
+				$value = $fieldToInsert['defaultValue'];
+			}
+			
+			$values[] = $value;
+		}
+
+		$keys[] = '#TELEGRAM_ID';
+		$values[] = $this->telegram_id;
+		
+		return str_replace($keys, $values, self::messageTemplate);
+	}
+
+	private function truncateBotOutputFile(){
+		$hFile = fopen($this->botOutputFile, 'r+');
+		assert($hFile !== false);
+
+		assert(ftruncate($hFile, 0));
+		assert(fclose($hFile));
+	}
+
+	public function send($message){
+		$json_msg = $this->fillTemplate(array('#TEXT' => $message));
+
+		$this->truncateBotOutputFile();
+		
+		try{
+			$this->bot->incomingUpdate(json_decode($json_msg)->message);
+		}
+		catch(TelegramException $tex){
+			$tex->release();
+		}
+
+		$response_json = file_get_contents($this->botOutputFile);
+		assert($response_json !== false);
+		assert(strlen($response_json) > 0);
+
+		$response = json_decode($response_json);
+		assert($response !== false);
+
+		return $response->text;
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/TelegramBotTest.php
+++ tests/TelegramBotTest.php
@@ -1,145 +1,86 @@
 <?php
 
-require_once(__DIR__.'/TelegramBotMockFactory.php');
+require_once(__DIR__.'/MessageTester.php');
 require_once(__DIR__.'/../config/stuff.php');
 
 class TelegramBotTest extends PHPUnit_Framework_TestCase{
-	const messageTemplate = '{
-		"update_id": 0,
-		"message": {
-			"message_id": 0,
-			"from": {
-			    "id": #TELEGRAM_ID,
-			    "first_name": "#FIRST_NAME",
-			    "username": "#USERNAME"
-			},
-			"chat": {
-			    "id": #CHAT_ID,
-			    "first_name": "#FIRST_NAME",
-			    "username": "#USERNAME",
-			    "type": "private"
-			},
-			"date": 0,
-			"text": "#TEXT"
-		}
-	}';
-	
-	const filedsToInsert = array(
-		array('key' => '#TEXT', 		'defaultValue' => null),
-		array('key' => '#FIRST_NAME', 	'defaultValue' => 'test first name'),
-		array('key' => '#USERNAME', 	'defaultValue' => 'test username'),
-		array('key' => '#TELEGRAM_ID', 	'defaultValue' => null),
-		array('key' => '#CHAT_ID', 		'defaultValue' => null)
-	);
-	
-	private function fillTemplate(array $fields){
-		$keys = array();
-		$values = array();
-		
-		foreach(self::filedsToInsert as $fieldToInsert){
-			$key = $fieldToInsert['key'];
-			$keys[] = $key;
-			$value = null;
-			
-			if(isset($fields[$key])){
-				$value = $fields[$key];
-			}
-			else{
-				if($fieldToInsert['defaultValue'] === null){
-					throw new Exception($fieldToInsert['key'].' is required');
-				}
-				
-				$value = $fieldToInsert['defaultValue'];
-			}
-			
-			$values[] = $value;
-		}
-		
-		return str_replace($keys, $values, self::messageTemplate);
+	const TEST_TELEGRAM_ID = 100500;
+	private $messageTester;
+
+	public function __construct(){
+		$this->messageTester = new MessageTester(self::TEST_TELEGRAM_ID);
 	}
-	
-	private function getMethod($name){
-		$rc = new ReflectionClass('TelegramBot');
-		$method = $rc->getMethod($name);
-		$method->setAccessible(true);
-		return $method;
+
+	private function userExists($telegram_id){
+		$pdo = createPDO();
+		$userExists = $pdo->prepare('
+			SELECT COUNT(*) FROM `users` WHERE `telegram_id` = :telegram_id
+		');
+
+		$userExists->execute(
+			array(
+				':telegram_id' => $telegram_id
+			)
+		);
+		
+		return intval($userExists->fetch()[0]) === 0;
 	}
 	
 	public function testRegistration(){
-		$tmpFilePath = tempnam('/tmp', 'TelegramBotTest::registrationTest');
 		
-		$botFactory = new TelegramBotMockFactory($tmpFilePath);
-		
-		$telegram_id = 100500;
-		$chat_id = 100500;
-		
-		$bot = $botFactory->createBot($telegram_id, $chat_id);
-		$getUserIdMethod = $this->getMethod('getUserId');
-		
-		$thrown = false;
-		
-		try{
-			$getUserIdMethod->invoke($bot, $telegram_id);
+		if($this->userExists(self::TEST_TELEGRAM_ID) === false){
+
+			$resp = $this->messageTester->send('/start');
+			$this->assertContains('Привет', $resp);
+
+			$this->assertTure($this->userExists(self::TEST_TELEGRAM_ID));
+
 		}
-		catch(TelegramException $tex){
-			$thrown = true;
+
+		$resp = $this->messageTester->send('/start');
+		$this->assertContains('знакомы', $resp);
+
+		$resp = $this->messageTester->send('/stop');
+		$this->assertContains('Ты уверен?', $resp);
+		
+		$resp = $this->messageTester->send('Нет');
+		$this->assertContains('Фух', $resp);
+
+		$resp = $this->messageTester->send('/stop');
+		$this->assertContains('Ты уверен?', $resp);
+
+		$resp = $this->messageTester->send('Да');
+		$this->assertContains('Прощай', $resp);
+
+
+		$this->assertFalse($this->userExists(self::TEST_TELEGRAM_ID));
+
+	}
+
+	public function testAddShow(){
+		
+		if($this->userExists(self::TEST_TELEGRAM_ID) === false){
+
+			$resp = $this->messageTester->send('/start');
+			$this->assertContains('Привет', $resp);
+
+			$this->assertTure($this->userExists(self::TEST_TELEGRAM_ID));
+
 		}
-		
-		$this->assertTrue($thrown);
-		
-		
-		$msg = $this->fillTemplate(
-			array(
-				'#TEXT' 		=> '/start',
-				'#FIRST_NAME'	=> 'firstName',
-				'#USERNAME'		=> 'username',
-				'#TELEGRAM_ID'	=> $telegram_id,
-				'#CHAT_ID'		=> $chat_id
-			)
-		);
-		
-		$bot->incomingUpdate(json_decode($msg)->message);
-		
-		$this->assertThat(
-			$getUserIdMethod->invoke($bot, $telegram_id),
-			$this->logicalNot(
-				$this->equalTo(null)
-			)
-		);
-		
-		
-		$msg = $this->fillTemplate(
-			array(
-				'#TEXT' 		=> '/stop',
-				'#FIRST_NAME'	=> 'firstName',
-				'#USERNAME'		=> 'username',
-				'#TELEGRAM_ID'	=> $telegram_id,
-				'#CHAT_ID'		=> $chat_id
-			)
-		);
-		
-		$bot->incomingUpdate(json_decode($msg)->message);
-		
-		$msg = $this->fillTemplate(
-			array(
-				'#TEXT' 		=> 'Да',
-				'#FIRST_NAME'	=> 'firstName',
-				'#USERNAME'		=> 'username',
-				'#TELEGRAM_ID'	=> $telegram_id,
-				'#CHAT_ID'		=> $chat_id
-			)
-		);
-		
-		$bot->incomingUpdate(json_decode($msg)->message);
-		
-		try{
-			$getUserIdMethod->invoke($bot, $telegram_id);
-		}
-		catch(TelegramException $tex){
-			$thrown = true;
-		}
-		
-		$this->assertTrue($thrown);
+
+
+		$resp = $this->messageTester->send('/add_show');
+		$this->assertContains('Как называется сериал?', $resp);
+
+		$showList = json_decode($resp);
+		$this->assertEquals('/cancel', $showList->keyboard[0][0]);
+
+
+		$randomShow = $showList->keyboard[rand(1, count($showList->keyboard) - 1)][rand(0, 1)];
+		$resp = $this->messageTester->send($randomShow);
+		$this->assertEquals($randomShow.' добавлен', $resp);
+
+
 	}
 }
 		
