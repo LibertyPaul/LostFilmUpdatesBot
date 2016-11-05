ada510975a798907dbbea2cdee4d1549ca776a30
LibertyPaul <admin@libertypaul.ru>
2016-11-05 16:31:30 +0000

    Brought in line with the interface

--- tests/TelegramBotMockFactory.php
+++ tests/TelegramBotMockFactory.php
@@ -5,21 +5,18 @@ require_once(__DIR__.'/../TelegramBot.php');
 require_once(__DIR__.'/HTTPRequesterMock.php');
 
 class TelegramBotMockFactory implements TelegramBotFactoryInterface{
-	private $outputPath;
 	
 	public function __construct($outputPath){
-		$this->setOutputPath($outputPath);
-	}
-	
-	public function setOutputPath($outputPath){
-		if(is_file($outputPath) === false){
-			throw new Exception("Invalid path $outputPath");
-		}
-		
+		assert(is_file($outputPath) === false);
 		$this->outputPath = $outputPath;
 	}
 	
-	public function createBot($telegram_id, $chat_id = null){
-		return new TelegramBot($telegram_id, $chat_id, new HTTPRequesterMock($this->outputPath), new Notifier($this));
+	public function createBot($telegram_id){
+		return new TelegramBot(
+			$telegram_id, 
+			new HTTPRequesterMock($this->outputPath),
+			new Notifier($this)
+		);
 	}
+
 }
