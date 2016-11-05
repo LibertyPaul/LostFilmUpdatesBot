f0e3c3d07806fddbd137f4aa940d86c7b2239ff3
LibertyPaul <admin@libertypaul.ru>
2016-11-05 14:32:41 +0000

    New/old series logic was moved to SeriesParserExecutor

--- SeriesParserExecutor.php
+++ SeriesParserExecutor.php
@@ -1,9 +1,47 @@
 <?php
+require_once(__DIR__.'/config/stuff.php');
 require_once(__DIR__."/SeriesParser.php");
 
-const rssURL = "http://www.lostfilm.tv/rssdd.xml";
-$seriesParser = new SeriesParser();
-$seriesParser->loadSrc(rssURL);
-$seriesParser->run();
+class SeriesParserExecutor{
+	private static $rssURL = "http://www.lostfilm.tv/rssdd.xml";
+	private $seriesParser;
+	private $addSeriesQuery;
+	
+	public function __construct(Parser $seriesParser){
+		assert($seriesParser !== null);
+		$this->seriesParser = $seriesParser;
+		
+		$pdo = createPDO();
+		
+		$this->addSeriesQuery = $pdo->prepare('
+			CALL addSeriesIfNotExist(
+				:showTitleRu,
+				:showTitleEn,
+				:seasonNumber,
+				:seriesNumber,
+				:seriesTitleRu,
+				:seriesTitleEn
+			);
+		');
 
+	}
 
+	public function run(){
+		$this->seriesParser->loadSrc(self::$rssURL);
+		$newSeriesList = $this->seriesParser->run();
+		
+		foreach($newSeriesList as $newSeries){
+			try{
+				$this->addSeriesQuery->execute($newSeries);
+			}
+			catch(Exception $ex){
+				echo __FILE__.':'.__LINE__."\t".$ex->getMessage().PHP_EOL;
+			}
+		}
+	}
+
+}
+
+$parser = new SeriesParser();
+$SPE = new SeriesParserExecutor($parser);
+$SPE->run();
