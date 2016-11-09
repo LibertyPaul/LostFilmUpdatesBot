187c67ebc7ffc9b96820be0a71c23c557d160207
LibertyPaul <admin@libertypaul.ru>
2016-11-09 21:51:03 +0000

    Fixed addSeriesIfNotExist procedure

--- DB_schema/patch1/addSeriesIfNotExist.sql
+++ DB_schema/patch1/addSeriesIfNotExist.sql
@@ -5,43 +5,38 @@ DROP PROCEDURE IF EXISTS addSeriesIfNotExist;
 CREATE PROCEDURE addSeriesIfNotExist(
 	IN showTitleRu VARCHAR(255),
 	IN showTitleEn VARCHAR(255),
-	IN seasonNumber INT(10) UNSIGNED,
-	IN seriesNumber INT(10) UNSIGNED,
+	IN season INT(10) UNSIGNED,
+	IN series INT(10) UNSIGNED,
 	IN seriesTitleRu TEXT,
 	IN seriesTitleEn TEXT
 )
-proc_body:BEGIN
-	DECLARE notFound INT DEFAULT FALSE;
-	DECLARE show_id INT(10) UNSIGNED;
+BEGIN
+	DECLARE showId INT(10) UNSIGNED;
 	DECLARE seriesCount INT(10) UNSIGNED;
-	DECLARE c_shows CURSOR FOR
+	
+	SET showId := (
 		SELECT	`id`
 		FROM	`shows` 
 		WHERE	`shows`.`title_ru`	LIKE showTitleRu 
-		AND		`shows`.`title_en`	LIKE showTitleEn;
-	DECLARE CONTINUE HANDLER FOR NOT FOUND SET notFound = TRUE;
+		AND		`shows`.`title_en`	LIKE showTitleEn
+	);
 	
-	OPEN c_shows;
-	FETCH c_shows INTO show_id;
-	CLOSE c_shows;
-	IF notFound THEN
-		LEAVE proc_body;
+	IF (showId IS NULL) THEN
+		SIGNAL SQLSTATE '02000';
 	END IF;
 
-	SET seriesCount = (
+	SET seriesCount := (
 		SELECT COUNT(*)
 		FROM	`series`
-		WHERE 	`show_id`		= show_id
-		AND		`seasonNumber`	= seasonNumber
-		AND		`seriesNumber`	= seriesNumber
+		WHERE 	`show_id`		= showId
+		AND		`seasonNumber`	= season
+		AND		`seriesNumber`	= series
 	);
 
-	IF seriesCount > 0 THEN
-		LEAVE proc_body;
+	IF seriesCount = 0 THEN
+		INSERT INTO `series` (show_id, seasonNumber, seriesNumber, title_ru, title_en)
+		VALUES (showId, season, series, seriesTitleRu, seriesTitleEn);
 	END IF;
-
-	INSERT INTO `series` (show_id, seasonNumber, seriesNumber, title_ru, title_en)
-	VALUES (show_id, seasonNumber, seriesNumber, seriesTitleRu, seriesTitleEn);
 END;
 //
 
