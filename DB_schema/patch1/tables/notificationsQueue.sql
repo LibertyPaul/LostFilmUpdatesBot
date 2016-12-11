7158be67b6ff3961d2e0032f5b761d6c12cd107d
LibertyPaul <admin@libertypaul.ru>
2016-12-11 20:17:36 +0000

    Fixed patch code

--- DB_schema/patch1/tables/notificationsQueue.sql
+++ DB_schema/patch1/tables/notificationsQueue.sql
@@ -2,10 +2,12 @@ CREATE TABLE `notificationsQueue` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `series_id` int(10) unsigned NOT NULL,
   `user_id` int(10) unsigned NOT NULL,
-  `responceCode` smallint(6) DEFAULT NULL,
+  `responseCode` smallint(6) DEFAULT NULL,
+  `retryCount` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `series_id_2` (`series_id`,`user_id`),
-  KEY `responceCode` (`responceCode`),
+  KEY `responseCode` (`responseCode`),
   KEY `series_id` (`series_id`),
-  KEY `user_id` (`user_id`)
+  KEY `user_id` (`user_id`),
+  KEY `retryCount` (`retryCount`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
