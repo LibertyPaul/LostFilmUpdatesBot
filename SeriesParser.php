32725dd55f2dcac7a7fe255998111591ace5f447
LibertyPaul <admin@libertypaul.ru>
2016-12-17 12:05:40 +0000

    Split-ny-dir rollback

---
+++ SeriesParser.php
@@ -0,0 +1,156 @@
+<?php
+require_once(__DIR__.'/config/stuff.php');
+require_once(__DIR__.'/Parser.php');
+require_once(__DIR__.'/Exceptions/StdoutTextException.php');
+require_once(__DIR__.'/Notifier.php');
+require_once(__DIR__.'/TelegramBotFactory.php');
+
+
+class FullSeasonWasFoundException extends Exception{}
+
+
+class SeriesParser extends Parser{
+	protected $rssData;
+	protected $notifier;
+	
+	protected $getUrlIdQuery;
+	protected $addSeries;
+	protected $latestSeriesQuery;
+
+	public function __construct(){
+		parent::__construct(null);
+	}
+
+	public function loadSrc($path){
+		parent::loadSrc($path);
+		$this->rssData = new SimpleXMLElement($this->pageSrc);
+	}
+			
+	protected function parseTitle($title){
+		$lastDotPos = strrpos($title, '.');
+		$seasonSeriesNumberTag = substr($title, $lastDotPos + 1);
+		$seasonSeriesNumberTag = trim($seasonSeriesNumberTag);
+		
+		$matches = array();
+		$res = preg_match('/S0?(\d+)E0?(\d+)/', $seasonSeriesNumberTag, $matches);
+		if($res !== 1){
+			$res = preg_match('/S\d+/', $seasonSeriesNumberTag, $matches);
+
+			if($res === 1){
+				throw new FullSeasonWasFoundException();
+			}
+
+			throw new StdoutTextException('seasonSeriesNumberTag parsing error `'.$seasonSeriesNumberTag.'`');
+		}
+		
+		$seasonNumber = $matches[1];
+		$seriesNumber = $matches[2];
+		
+
+		$title = substr($title, 0, $lastDotPos);
+		$formatEndPos = strrpos($title, ']');
+		
+		$formatTag = null;
+		if($formatEndPos !== false){
+			$formatStartPos = findMatchingParenthesis($title, $formatEndPos);
+			if($formatStartPos === false){
+				throw new StdoutTextException('Broken format tag (opening parenthesis not found)');
+			}
+
+			$length = $formatEndPos - $formatStartPos - 1;
+			$formatTag = substr($title, $formatStartPos + 1, $length);
+			$title = substr($title, 0, $formatStartPos);
+			$title = rtrim($title);
+		}
+		
+		$seriesNameEn = null;
+		$seriesNameEnEndPos = strlen($title) - 1;
+		if($title[$seriesNameEnEndPos] === ')'){
+			$seriesNameEnStartPos = findMatchingParenthesis($title, $seriesNameEnEndPos);
+			if($seriesNameEnStartPos === false){
+				throw new StdoutTextException("Broken series name (opening parenthesis not found)");
+			}
+
+			$length = $seriesNameEnEndPos - $seriesNameEnStartPos - 1;
+			$seriesNameEn = substr($title, $seriesNameEnStartPos + 1, $length);
+			$title = substr($title, 0, $seriesNameEnStartPos);
+		}
+		
+		$seriesNameRu = null;
+		$offset = 2;
+		
+		$seriesNameRuStartPos = strpos($title, ').');
+		if($seriesNameRuStartPos === false){
+			$seriesNameRuStartPos = strpos($title, '.');
+			$offset = 1;
+			if($seriesNameRuStartPos === false){
+				throw new StdoutTextException("Start of show ru name wasn't found");
+			}
+		}
+			
+		$seriesNameRu = substr($title, $seriesNameRuStartPos + $offset);
+		$seriesNameRu = trim($seriesNameRu);
+		$title = substr($title, 0, $seriesNameRuStartPos + 1); // + 1 to left closing parenthesis
+	
+	
+		$showNameEn = null;
+	
+		$showNameEnEndPos = strrpos($title, ')');
+		if($showNameEnEndPos !== false){
+			$showNameEnStartPos = findMatchingParenthesis($title, $showNameEnEndPos);
+			if($showNameEnStartPos === false){
+				throw new StdoutTextException('Broken show name (opening parenthesis not found)');
+			}
+			
+			$length = $showNameEnEndPos - $showNameEnStartPos - 1;
+			$showNameEn = substr($title, $showNameEnStartPos + 1, $length);
+			$title = substr($title, 0, $showNameEnStartPos);
+		}
+		
+		$title = trim($title);
+		$showNameRu = $title;
+		
+		return array(
+			'showTitleRu'	=> $showNameRu,
+			'showTitleEn' 	=> $showNameEn,
+			'seriesTitleRu' => $seriesNameRu,
+			'seriesTitleEn' => $seriesNameEn,
+			'seasonNumber' 	=> intval($seasonNumber),
+			'seriesNumber' 	=> intval($seriesNumber)
+		);
+	}
+		
+	
+	public function run(){
+		$result = array(); // [seriesTitleRu, seriesTitleEn, seasonNumber, seriesNumber]
+		
+		foreach($this->rssData->channel->item as $item){
+			try{
+				$result[] = $this->parseTitle($item->title);
+			}
+			catch(FullSeasonWasFoundException $ex){
+				echo 'Full season entry was found. Skipping'.PHP_EOL;
+			}
+			catch(Exception $ex){
+				echo "[ERROR]".$ex->getMessage().PHP_EOL;
+			}
+		}
+		
+		return $result;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ShowParser.php
@@ -0,0 +1,126 @@
+<?php
+require_once(__DIR__.'/Parser.php');
+require_once(__DIR__.'/config/stuff.php');
+require_once(__DIR__.'/Exceptions/StdoutTextException.php');
+require_once(__DIR__.'/ShowAboutParser.php');
+
+
+class ShowParser extends Parser{
+	protected $getShowIdQuery;
+	protected $addShowQuery;
+	protected $updateShowStateQuery;
+	protected $showAboutParser;
+	
+	const showPageTemplate = 'https://www.lostfilm.tv/browse.php?cat=#url_id';
+
+	public function __construct($pageEncoding = "utf-8"){
+		parent::__construct($pageEncoding);
+		$pdo = createPDO();
+		$this->showAboutParser = new ShowAboutParser('CP1251');
+		
+		$this->getShowIdQuery = $pdo->prepare('
+			SELECT `id`
+			FROM `shows`
+			WHERE	STRCMP(`title_ru`, :title_ru) = 0
+			AND		STRCMP(`title_en`, :title_en) = 0
+		');
+		
+		$this->addShowQuery = $pdo->prepare('
+			INSERT INTO `shows` (title_ru, title_en, onAir)
+			VALUES (:title_ru, :title_en, :onAir)
+		');
+		
+		$this->updateShowStateQuery = $pdo->prepare('
+			UPDATE `shows`
+			SET `onAir` = :onAir
+			WHERE `id` = :id
+		');
+	}
+	
+	protected function getShowId($title_ru, $title_en){
+		$this->getShowIdQuery->execute(
+			array(
+				':title_ru' => $title_ru,
+				':title_en' => $title_en
+			)
+		);
+		
+		$res = $this->getShowIdQuery->fetch(PDO::FETCH_ASSOC);
+		if($res === false){
+			return null;
+		}
+		
+		return $res['id'];
+	}
+	
+	protected function parseShowList(){ // -> array(url_id => array(title_ru => '', title_en => ''), ...)
+		$regexp = '/<a href="\/browse\.php\?cat=_?(\d+)" class="bb_a">([^<]*)<br><span>\(([^(]*)\)<\/span><\/a>/';
+		$matches = array();
+		$matchesCount = preg_match_all($regexp, $this->pageSrc, $matches);
+		if($matchesCount === false){
+			throw new Exception("preg_match_all error: ".preg_last_error());
+		}
+		
+		$result = array();
+		
+		for($i = 0; $i < $matchesCount; ++$i){
+			$url_id	= intval($matches[1][$i]);
+			$title_ru = $matches[2][$i];
+			$title_en = $matches[3][$i];
+			
+			$result[$url_id] = array(
+				'title_ru' => $title_ru,
+				'title_en' => $title_en
+			);
+		}
+		
+		return $result;
+	}
+	
+	protected function getShowPageURL($url_id){
+		if(is_int($url_id) === false){
+			throw new StdoutTextException('$url_id should be of an integer type.');
+		}
+		
+		return str_replace('#url_id', $url_id, self::showPageTemplate);
+	}
+	
+	private function isOnAir($url_id){
+		$url = $this->getShowPageURL($url_id);
+		$this->showAboutParser->loadSrc($url);
+		return $this->showAboutParser->run();
+	}
+	
+	public function run(){
+		$showList = $this->parseShowList();
+		foreach($showList as $url_id => $titles){
+			try{
+				$showId = $this->getShowId($titles['title_ru'], $titles['title_en']);
+				if($showId === null){
+					echo "New show: $titles[title_ru] ($titles[title_en])".PHP_EOL;
+					
+					$onAir = $this->isOnAir($url_id) ? 'Y' : 'N';
+					
+					$this->addShowQuery->execute(
+						array(
+							':title_ru' => $titles['title_ru'],
+							':title_en' => $titles['title_en'],
+							':onAir'	=> $onAir
+						)
+					);
+				}
+			}
+			catch(Exception $ex){
+				echo '[EXCEPTION]: '.$ex->getMessage().PHP_EOL;
+			}
+		}
+	}
+}
+	
+
+
+
+
+
+
+
