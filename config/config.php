74eca12c5e83182fbf4843642f2d4b819d337ac3
LibertyPaul <admin@libertypaul.ru>
2017-01-16 22:23:10 +0000

    Some settings were moved to bot_specific_info.php

--- config/config.php
+++ config/config.php
@@ -6,10 +6,4 @@ const BOTAN_URL = 'https://api.botan.io/track';
 const MEMCACHE_STORE_TIME = 86400;//1 day
 const MAX_NOTIFICATION_RETRY_COUNT = 5;
 
-
 date_default_timezone_set('UTC');
-
-$debug = false;
-
-const PERFORM_ACTUAL_MESSAGE_SEND = false; // if false - no messages will actaully be sent to the Tegegram API, instead they will be stored in file
-const UNDELIVERED_MESSAGE_STORAGE = __DIR__.'/../logs/UndeliviredMessages.log';
