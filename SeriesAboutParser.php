e3dd03b916bdfebba6c482e196b8d14a35daef8d
LibertyPaul <admin@libertypaul.ru>
2017-02-05 20:17:28 +0000

    SeriesAboutParser now throws SeriesIsNotPublishedYet exception when eposode was pushed to RSS but is not published on site

--- SeriesAboutParser.php
+++ SeriesAboutParser.php
@@ -4,10 +4,12 @@ require_once(__DIR__.'/Tracer.php');
 require_once(__DIR__.'/Parser.php');
 require_once(__DIR__.'/HTTPRequesterInterface.php');
 
+class SeriesIsNotPublishedYet extends RuntimeException{}
 
 class SeriesAboutParser extends Parser{
 	private $tracer;
-
+	
+	const expected_tag_regex = '/<div class="expected">([\s\S]*?)<\/div>/';
 	const title_ru_regex = '/<div class="title-ru">([\s\S]*?)<\/div>/';
 	const title_en_regex = '/<div class="title-en">([\s\S]*?)<\/div>/';
 	
@@ -24,31 +26,38 @@ class SeriesAboutParser extends Parser{
 		$matchesCount = preg_match($regex, $src, $matches);
 		if($matchesCount === false){
 			$this->tracer->log('[ERROR]', __FILE__, __LINE__, 'preg_match has failed with code: '.preg_last_error());
-			$this->tracer->log('[ERROR]', __FILE__, __LINE__, PHP_EOL.$this->pageSrc);
+			$this->tracer->log('[ERROR]', __FILE__, __LINE__, "Regex string: '$regex'");
+			$this->tracer->log('[ERROR]', __FILE__, __LINE__, 'Source:'.PHP_EOL.$this->pageSrc);
 			throw new Exception('preg_match has failed');
 		}
-
-		if($matchesCount === 0){
-			$this->tracer->log('[DATA ERROR]', __FILE__, __LINE__, "Page src doesn't match pattern");
-			$this->tracer->log('[ERROR]', __FILE__, __LINE__, PHP_EOL.$this->pageSrc);
-			throw new Exception("Link doesn't match pattern");
-		}
-
-		assert($matchesCount === 1);
 
 		return $matches;
 	}
 	
 	public function run(){
+		$expected_res = $this->singleRegexSearch(self::expected_tag_regex, $this->pageSrc);
+		if(empty($expected_res) === false){
+			throw new SeriesIsNotPublishedYet('Series will be published a bit later');
+		}
+		
+
 		$title_ru_res = $this->singleRegexSearch(self::title_ru_regex, $this->pageSrc);
-		$title_ru = $title_ru_res[1];
+		if(empty($title_ru_res)){
+			$this->tracer->log('[ERROR]', __FILE__, __LINE__, "title-ru tag wasn't found on the page");
+			$this->tracer->log('[ERROR]', __FILE__, __LINE__, 'Source:'.PHP_EOL.$this->pageSrc);
+			throw new RuntimeException("title-ru tag wasn't found on the page");
+		}
 
 		$title_en_res = $this->singleRegexSearch(self::title_en_regex, $this->pageSrc);
-		$title_en = $title_en_res[1];
+		if(empty($title_en_res)){
+			$this->tracer->log('[ERROR]', __FILE__, __LINE__, "title-en tag wasn't found on the page");
+			$this->tracer->log('[ERROR]', __FILE__, __LINE__, 'Source:'.PHP_EOL.$this->pageSrc);
+			throw new RuntimeException("title-en tag wasn't found on the page");
+		}
 
 		return array(
-			'title_ru' => $title_ru,
-			'title_en' => $title_en
+			'title_ru' => $title_ru_res[1],
+			'title_en' => $title_en_res[1]
 		);
 	}
 }
