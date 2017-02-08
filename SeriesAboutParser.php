7d5f7b6fa466a7c8fdabf13bb3b15e15dec14d65
LibertyPaul <admin@libertypaul.ru>
2017-02-08 19:37:06 +0000

    "Is series ready" condotion improvement

--- SeriesAboutParser.php
+++ SeriesAboutParser.php
@@ -9,7 +9,8 @@ class SeriesIsNotPublishedYet extends RuntimeException{}
 class SeriesAboutParser extends Parser{
 	private $tracer;
 	
-	const expected_tag_regex = '/<div class="expected">([\s\S]*?)<\/div>/';
+	const discussion_tag = '/<a href="\/series\/\w+\/season_\d+\/episode_\d+\/comments" class="item last">Обсуждение серии<\/a>/';
+	const expected_tag_regex = '/<div class="expected">/';
 	const title_ru_regex = '/<div class="title-ru">([\s\S]*?)<\/div>/';
 	const title_en_regex = '/<div class="title-en">([\s\S]*?)<\/div>/';
 	
@@ -21,9 +22,9 @@ class SeriesAboutParser extends Parser{
 		$this->tracer = new Tracer(__CLASS__);
 	}
 
-	private function singleRegexSearch($regex, $src){
+	private function singleRegexSearch($regex){
 		$matches = array();
-		$matchesCount = preg_match($regex, $src, $matches);
+		$matchesCount = preg_match($regex, $this->pageSrc, $matches);
 		if($matchesCount === false){
 			$this->tracer->log('[ERROR]', __FILE__, __LINE__, 'preg_match has failed with code: '.preg_last_error());
 			$this->tracer->log('[ERROR]', __FILE__, __LINE__, "Regex string: '$regex'");
@@ -35,24 +36,31 @@ class SeriesAboutParser extends Parser{
 	}
 	
 	public function run(){
-		$expected_res = $this->singleRegexSearch(self::expected_tag_regex, $this->pageSrc);
+		$expected_res = $this->singleRegexSearch(self::expected_tag_regex);
 		if(empty($expected_res) === false){
-			throw new SeriesIsNotPublishedYet('Series will be published a bit later');
+			throw new SeriesIsNotPublishedYet('Series seems not to be ready yet. '.self::expected_tag_regex.' was found');
 		}
-		
 
-		$title_ru_res = $this->singleRegexSearch(self::title_ru_regex, $this->pageSrc);
+		$title_ru_res = $this->singleRegexSearch(self::title_ru_regex);
 		if(empty($title_ru_res)){
-			$this->tracer->log('[ERROR]', __FILE__, __LINE__, "title-ru tag wasn't found on the page");
-			$this->tracer->log('[ERROR]', __FILE__, __LINE__, 'Source:'.PHP_EOL.$this->pageSrc);
-			throw new RuntimeException("title-ru tag wasn't found on the page");
+			throw new SeriesIsNotPublishedYet('Series seems not to be ready yet. '.self::title_ru_regex.' was not found');
 		}
 
-		$title_en_res = $this->singleRegexSearch(self::title_en_regex, $this->pageSrc);
+		$title_en_res = $this->singleRegexSearch(self::title_en_regex);
 		if(empty($title_en_res)){
-			$this->tracer->log('[ERROR]', __FILE__, __LINE__, "title-en tag wasn't found on the page");
-			$this->tracer->log('[ERROR]', __FILE__, __LINE__, 'Source:'.PHP_EOL.$this->pageSrc);
-			throw new RuntimeException("title-en tag wasn't found on the page");
+			throw new SeriesIsNotPublishedYet('Series seems not to be ready yet. '.self::title_en_regex.' was not found');
+		}
+
+		$title_ru = $title_ru_res[1];
+		$title_en = $title_en_res[1];
+
+		if($title_ru === $title_en){ // Either series wasn't published or title_ru is the same as tile_en
+			// Trying to determine
+
+			$nextLinkActive = $this->singleRegexSearch(self::discussion_tag);
+			if(empty($nextLinkActive)){
+				throw new SeriesIsNotPublishedYet('Series seems not to be ready yet. '.self::discussion_tag.' was not found');
+			}
 		}
 
 		return array(
