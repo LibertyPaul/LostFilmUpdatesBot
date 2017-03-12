abad50c12614f0d7db797e0ef638b39ed866907c
LibertyPaul <admin@libertypaul.ru>
2017-03-12 14:29:48 +0000

    Fixed patch execution order

--- DB_schema/deploy_all.sh
+++ DB_schema/deploy_all.sh
@@ -4,10 +4,22 @@ schema_dir=$(dirname $0)
 
 "./$schema_dir/insertValues.sh" .
 
-for patch in $(find "$schema_dir" -maxdepth 1 ! -path . -type d | sort -u); do
-	"./$schema_dir/deploy.sh" "$patch"
+declare -a patch_order=()
 
-	if [ "$?" != 0 ]; then
+patch_order+=("initial_schema")
+
+patch_id=1
+while [ -d "$schema_dir/patch$patch_id" ]; do
+	patch_order+=("patch$patch_id")
+	let patch_id+=1
+done
+
+for patch in ${patch_order[@]}; do
+	"$schema_dir/deploy.sh" "$schema_dir/$patch"
+
+	if [ "$?" == 0 ]; then
+		echo "$patch -- Success."
+	else
 		echo "Failed to install $schema_dir/$patch. Aborting."
 		exit 1
 	fi
