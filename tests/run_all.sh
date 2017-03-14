910663f2f6ab6ed05c64f59a5ffa56e5e53112cb
LibertyPaul <admin@libertypaul.ru>
2017-03-14 21:34:44 +0000

    Syntax of every .php file will be checked during tests/run_all.sh

--- tests/run_all.sh
+++ tests/run_all.sh
@@ -1,6 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-for test in $(ls *Test.php); do
+readonly dir=$(dirname "$0")
+
+for php_file in $(find "$dir/.." -name "*.php" -type f); do
+	php -l "$php_file"
+	if [ "$?" != 0 ]; then
+		echo "Aborting"
+		exit 1
+	fi
+done
+
+for test in $(find "$dir" -name "*Test.php" -type f); do
 	echo "###### Testing $test... ######"
 	phpunit "$test"
 	echo "###### Finished $test.  ######"
