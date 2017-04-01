9f3d0f078955c1e97d01d2450d9472989fe6b24e
LibertyPaul <admin@libertypaul.ru>
2017-04-01 21:09:00 +0100

    Trace Levels and Standalone storage added to Tracer

--- Tracer.php
+++ Tracer.php
@@ -3,11 +3,8 @@ require_once(__DIR__.'/TracerBase.php');
 require_once(__DIR__.'/config/stuff.php');
 
 class Tracer extends TracerBase{
-	private $hEventLog = null;
-	const eventLogExtension = 'event.log';
-
-	private $hErrorLog = null;
-	const errorLogExtension = 'error.log';
+	private $hFile = null;
+	const logsDir = __DIR__.'/logs/';
 
 	public function __construct($traceName){
 		parent::__construct($traceName);
@@ -15,71 +12,69 @@ class Tracer extends TracerBase{
 
 	public function __destruct(){
 		parent::__destruct();
-		if($this->hEventLog !== null){
-			assert(fclose($this->hEventLog));
-		}
 
-		if($this->hErrorLog !== null){
-			assert(fclose($this->hErrorLog));
+		if($this->hFile !== null){
+			assert(fclose($this->hFile));
 		}
 	}
 
-	private static function openFile($path){
-		$traceDir = dirname($path);
-		
-		if(file_exists($traceDir)){
-			if(is_dir($traceDir) === false){
-				syslog(LOG_CRIT, "[TRACER] logs dir is not a directory ($traceDir)");
-				throw new Exception("Unable to open $traceDir directory");
+	private static function createDirIfNotExists($dir){
+		if(file_exists($dir)){
+			if(is_dir($dir) === false){
+				TracerBase::syslogCritical('[SETUP]', __FILE__, __LINE__, "logs dir is not a directory ($dir)");
+				throw new Exception("Unable to open $dir directory");
 			}
-			$traceExists = file_exists($path);
 		}
 		else{
-			assert(mkdir($traceDir, 0777, true));
-			$traceExists = false;
+			assert(mkdir($dir, 0777, true));
 		}
+	}
+		
 
-		if($traceExists === false){
+	private static function prepareTraceFile($path){
+		self::createDirIfNotExists(dirname($path));
+	
+		if(file_exists($path) === false){
 			assert(touch($path));
 			assert(chmod($path, 0666));
 		}
 		
-		
 		$hFile = fopen($path, 'a');
 		if($hFile === false){
-			syslog(LOG_CRIT, "[Tracer] Unable to open file '$path'");
+			TracerBase::syslogCritical('[SETUP]', __FILE__, __LINE__, "Unable to open file '$path'");
 			throw new Exception("Unable to open $path file.".PHP_EOL.print_r(error_get_last(), true));
 		}
 
 		return $hFile;
 	}
 
-	private function getPath($extension){
-		return __DIR__.'/logs/'.$this->traceName.".$extension";
+	protected function storeStandalone($text){
+		assert(is_string($text));
+
+		$fName = $this->traceName.'.'.uniqid(rand(), true).'.txt';
+		$fPath = self::logsDir."standalone/$fName";
+		
+		$hFile = self::prepareTraceFile($fPath);
+
+		assert(fwrite($hFile, $text));
+		assert(fclose($hFile));
+		
+		$this->write("<... Stored to standalone $fName ...>");
 	}
 
-	private function write($text, $hFile){
+	protected function write($text){
 		assert(is_string($text));
-		assert($hFile !== null);
 
+		if($this->hFile === null){
+			$this->hFile = self::prepareTraceFile(self::logsDir.$this->traceName.'.log');
+		}
+		
 		$text .= PHP_EOL;
 		
-		assert(fwrite($hFile, $text));
+		assert(flock($this->hFile, LOCK_EX));
+		assert(fwrite($this->hFile, $text));
+		assert(flock($this->hFile, LOCK_UN));
+
 	}
 
-	protected function writeEvent($text){
-		if($this->hEventLog === null){
-			$this->hEventLog = self::openFile($this->getPath(self::eventLogExtension));
-		}
-		
-		$this->write($text, $this->hEventLog);
-	}
-
-	protected function writeError($text){
-		if($this->hErrorLog === null){
-			$this->hErrorLog = self::openFile($this->getPath(self::errorLogExtension));
-		}
-
-		$this->write($text, $this->hErrorLog);
-	}
 }
--- TracerBase.php
+++ TracerBase.php
@@ -1,37 +1,86 @@
 <?php
 
+abstract class TracerLevel{
+	const Critical		= 0;
+	const Error			= 1;
+	const Warning		= 4;
+	const Notice		= 6;
+	const Event			= 9;
+
+	private static $levelMap = array(
+		'CRITICAL'	=> self::Critical,
+		'ERROR'		=> self::Error,
+		'WARNING'	=> self::Warning,
+		'NOTICE'	=> self::Notice,
+		'EVENT'		=> self::Event
+	);
+
+//	private static $codeMap = array_flip(self::$levelMap);
+	private static $codeMap = array(
+		self::Critical	=> 'CRITICAL',
+		self::Error		=> 'ERROR',
+		self::Warning	=> 'WARNING',
+		self::Notice	=> 'NOTICE',
+		self::Event		=> 'EVENT'
+	);
+
+	public static function logEverythingLevel(){
+		return self::Event;
+	}
+
+	public static function getLevelByName($name){
+		if(array_key_exists($name, self::$levelMap)){
+			return self::$levelMap[$name];
+		}
+		else{
+			throw OutOfBoundsException("Invalid trace level name: '$name'");
+		}
+	}
+
+	public static function getNameByLevel($level){
+		if(array_key_exists($level, self::$codeMap)){
+			return self::$codeMap[$level];
+		}
+		else{
+			throw OutOfBoundsException("Invalid trace level: '$level'");
+		}
+	}
+}
+
 abstract class TracerBase{
 	protected $traceName;
+	private $maxLevel;
+	private $secondTracer;
 
-	protected function __construct($traceName){
+	protected function __construct($traceName, TracerBase $secondTracer = null){
 		assert(is_string($traceName));
 		$this->traceName = $traceName;
-
-		if(TRACER_LOG_START_END){
-			$this->logEvent(
-				'[TRACER]',
-				__FILE__,
-				__LINE__,
-				'Trace '.$this->traceName.' has been started'
-			);
+		$this->secondTracer = $secondTracer;
+			
+		if(defined('TRACER_LEVEL')){
+			$this->maxLevel = TracerLevel::getLevelByName(TRACER_LEVEL);
 		}
+		else{
+			$this->maxLevel = TracerLevel::logEverythingLevel();
+			$this->log('WARNING', '[TRACER]', __FILE__, __LINE__, 'TRACER_LEVEL is not set. Logging everything.');
+		}
+
+
+		$this->logEvent('[TRACER]', __FILE__, __LINE__, 'Started.');
+
 	}
 
 	public function __destruct(){
-		if(TRACER_LOG_START_END){
-			$this->logEvent(
-				'[TRACER]',
-				__FILE__,
-				__LINE__,
-				'Trace '.$this->traceName.' has been ended'
-			);
-		}
+		$this->logEvent('[TRACER]', __FILE__, __LINE__, 'Ended.');
 	}
 
-	abstract protected function writeEvent($text);
-	abstract protected function writeError($text);
+	abstract protected function write($text);
 
-	private static function compileRecord($tag, $file, $line, $message){
+	protected function storeStandalone($text){
+		$this->write($text); // default behavior for case we don't want to use this feature
+	}
+
+	private static function compileRecord($level, $tag, $file, $line, $message){
 		assert(is_string($tag));
 		assert(is_string($file));
 		assert(is_int($line));
@@ -40,7 +89,7 @@ abstract class TracerBase{
 		$date = date('Y.m.d H:i:s');
 		
 		// basename should never fail on any input
-		$record = "$tag $date ".basename($file).":$line";		
+		$record = "$level\t$tag $date ".basename($file).":$line";		
 		
 		if($message !== null){
 			$record .= "\t$message";
@@ -49,22 +98,57 @@ abstract class TracerBase{
 		return $record;
 	}
 
-	public function logEvent($tag, $file, $line, $message = null){
-		$record = self::compileRecord($tag, $file, $line, $message);
-		$this->writeEvent($record);
+	public function log($level, $tag, $file, $line, $message = null){
+		$messageLevel = TracerLevel::getLevelByName($level);
+
+		if($messageLevel <= $this->maxLevel){
+			$record = self::compileRecord($level, $tag, $file, $line, $message);
+			
+			if(defined('TRACER_COPMPESSION_BOUND') && strlen($record) > TRACER_COPMPESSION_BOUND){
+				try{
+					$this->storeStandalone($record);
+				}
+				catch(RuntimeException $ex){
+					$this->logException('[TRACER]', $ex);
+					$this->write($record);
+				}	
+			}
+			else{
+				$this->write($record);
+			}
+		}
+
+		if($this->secondTracer !== null){
+			$this->secondTracer->log($level, $tag, $file, $line, $message);
+		}
+	}
+
+	public function logCritical($tag, $file, $line, $message = null){
+		$this->log('CRITICAL', $tag, $file, $line, $message);
 	}
 
 	public function logError($tag, $file, $line, $message = null){
-		$record = self::compileRecord($tag, $file, $line, $message);
-		$this->writeError($record);
+		$this->log('ERROR', $tag, $file, $line, $message);
+	}
+
+	public function logWarning($tag, $file, $line, $message = null){
+		$this->log('WARNING', $tag, $file, $line, $message);
+	}
+
+	public function logEvent($tag, $file, $line, $message = null){
+		$this->log('EVENT', $tag, $file, $line, $message);
+	}
+
+	public function logNotice($tag, $file, $line, $message = null){
+		$this->log('NOTICE', $tag, $file, $line, $message);
 	}
 
 	public function logException($tag, Exception $exception){
-		return $this->logError(
-			$tag,
-			$exception->getFile(),
-			$exception->getLine(),
-			$exception->getMessage()
-		);
+		$this->logError($tag, $exception->getFile(), $exception->getLine(), $exception->getMessage());
+	}
+
+	public static function syslogCritical($tag, $file, $line, $message = null){
+		$record = self::compileRecord('CRITICAL', $tag, $file, $line, $message);
+		assert(syslog(LOG_CRIT, $record));
 	}
 }
