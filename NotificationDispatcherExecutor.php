e667487d48d157a2057d0ab6ea15f71f8743152d
LibertyPaul <admin@libertypaul.ru>
2017-04-01 21:08:01 +0100

    Exception/Error handlers added to NotificationDispatcherExecutor.php

--- NotificationDispatcherExecutor.php
+++ NotificationDispatcherExecutor.php
@@ -1,4 +1,6 @@
 <?php
+require_once(__DIR__.'/ErrorHandler.php');
+require_once(__DIR__.'/ExceptionHandler.php');
 
 require_once(__DIR__.'/TelegramBotFactory.php');
 require_once(__DIR__.'/Notifier.php');
