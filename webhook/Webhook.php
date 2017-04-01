51969226aa323f33fd4a76a0054957b19c9b35b4
LibertyPaul <admin@libertypaul.ru>
2017-04-01 21:05:22 +0100

    Refactored webhook

---
+++ webhook/Webhook.php
@@ -0,0 +1,135 @@
+<?php
+require_once(__DIR__.'/../config/config.php');
+require_once(__DIR__.'/../config/stuff.php');
+require_once(__DIR__.'/../TelegramBotFactoryInterface.php');
+require_once(__DIR__.'/../UpdateHandler.php');
+
+require_once(__DIR__.'/../Tracer.php');
+
+require_once(__DIR__.'/../HTTPRequester.php');
+
+
+abstract class WebhookReasons{
+	const OK				= 0;
+	const invalidPassword	= 1;
+	const formatError		= 2;
+	const failed			= 3;
+}
+
+class Webhook{
+	private $incomingLog;
+	private $tracer;
+	private $updateHandler;
+
+	public function __construct(UpdateHandler $updateHandler){
+		assert($updateHandler !== null);
+		$this->updateHandler = $updateHandler;
+
+		try{
+			$this->tracer = new Tracer(__CLASS__);
+			$this->incomingLog = new Tracer('incomingMessages');
+		}
+		catch(Exception $ex){
+			TracerBase::syslogCritical('[TRACER]', __FILE__, __LINE__, 'Unable to create Tracer instance');
+		}
+	}
+
+	private function verifyPassword($password){
+		if(defined('WEBHOOK_PASSWORD') === false){
+			$this->tracer->logNotice('[SECURITY]', __FILE__, __LINE__, 'Webhook password is not set. Check was skipped.');
+			return true;
+		}
+
+		return $password === WEBHOOK_PASSWORD;
+	}
+
+	private function respondFinal($reason){
+		switch($reason){
+			case WebhookReasons::OK:
+				http_response_code(200);
+				echo 'Accepted. Processed.'.PHP_EOL;
+				break;
+
+			case WebhookReasons::invalidPassword:
+				http_response_code(401);
+				echo 'Invalid password. Try 123456.'.PHP_EOL;
+				break;
+
+			case WebhookReasons::formatError:
+				http_response_code(400);
+				echo 'Format error.';
+				break;
+
+			case WebhookReasons::failed:
+				http_response_code(500);
+				echo 'Failed for some reason.'.PHP_EOL;
+				break;
+
+			default:
+				$this->tracer->logError('[UNKNOWN REASON]', __FILE__, __LINE__, $reason);
+				echo 'hmm...'.PHP_EOL;
+				http_response_code(200);
+		}
+	}
+
+	private function logUpdate($update){
+		$prettyJSON = json_encode($update, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+		if($prettyJSON === false){
+			$this->tracer->logError('[JSON]', __FILE__, __LINE__, 'json_encode error: '.json_last_error_msg());
+			$this->tracer->logNotice('[INFO]', __FILE__, __LINE__, PHP_EOL.print_r($update, true));
+			return;
+		}
+
+		$this->incomingLog->logEvent('[INCOMING MESSAGE]', __FILE__, __LINE__, PHP_EOL.$prettyJSON);
+	}
+
+	public function processUpdate($password, $postData){
+		if($this->verifyPassword($password) === false){
+			$this->tracer->logNotice('[SECURITY]', __FILE__, __LINE__, "Incorrect password: '$password'");
+			$this->tracer->logNotice('[INFO]', __FILE__, __LINE__, PHP_EOL.$postData);
+			$this->respondFinal(WebhookReasons::invalidPassword);
+			return;
+		}
+
+		$update = json_decode($postData);
+		if($update === null){
+			$this->tracer->logError('[JSON]', __FILE__, __LINE__, 'json_decode error: '.json_last_error_msg());
+			$this->tracer->logNotice('[INFO]', __FILE__, __LINE__, PHP_EOL.$postData);
+			$this->respondFinal(WebhookReasons::formatError);
+			return;
+		}
+
+		$this->logUpdate($update);
+
+		try{
+			$this->updateHandler->handleUpdate($update);
+			$this->respondFinal(WebhookReasons::OK);
+		}
+		catch(Exception $ex){
+			$this->tracer->logException($ex);
+			$this->respondFinal(WebhookReasons::failed);
+		}
+				
+		if(defined('MESSAGE_STREAM_URL')){
+			try{
+				$this->resendUpdate($postData, MESSAGE_STREAM_URL);
+			}
+			catch(Exception $ex){
+				$this->tracer->logException($ex);
+			}
+		}
+	}
+
+	private function resendUpdate($postData, $URL){
+		$testStream = new HTTPRequester();
+
+		if(defined('MESSAGE_STREAM_PASSWORD')){
+			$URL .= '?password='.MESSAGE_STREAM_PASSWORD;
+		}
+		
+		$res = $testStream->sendJSONRequest($URL, $postData);
+		if($res['code'] >= 400){
+			$this->tracer->logError('[STREAM]', __FILE__, __LINE__, 'message resend has failed with code '.$res['code']);
+		}
+	}
+}
--- webhook/webhook.php
+++ webhook/webhook.php
@@ -1,68 +1,17 @@
 <?php
-require_once(__DIR__.'/../config/config.php');
-require_once(__DIR__.'/../config/stuff.php');
+require_once(__DIR__.'/../ErrorHandler.php');
+require_once(__DIR__.'/../ExceptionHandler.php');
+
+require_once(__DIR__.'/../TelegramBotFactory.php');
 require_once(__DIR__.'/../UpdateHandler.php');
-
-require_once(__DIR__.'/../Tracer.php');
-require_once(__DIR__.'/../EchoTracer.php');
-
-require_once(__DIR__.'/../HTTPRequester.php');
-
-$tracer = null;
-try{
-	$tracer = new Tracer('Webhook');
-}
-catch(Exception $ex){
-	$tracer = new EchoTracer();
-	$tracer->logException('[TRACER CRITICAL]'. $ex);
-}
-
-function exception_handler($ex){
-	global $tracer;
-	$tracer->logException('[ERROR]', $ex);
-}
-
-
-function error_handler($errno, $errstr, $errfile, $errline, $errcontext){
-	global $tracer;
-	$tracer->logError('[ERROR]', $errfile, $errline, "($errno)\t$errstr");
-}
-
-set_error_handler('error_handler');
-set_exception_handler('exception_handler');
-
-$update_json = file_get_contents('php://input');
-
-if(isset($_GET['password']) === false){
-	$tracer->logError('[ERROR]', __FILE__, __LINE__, 'No password provided');
-	$tracer->logError('[ERROR]', __FILE__, __LINE__, PHP_EOL.print_r($_REQUEST, true));
-	exit('no password provided');
-}
-elseif($_GET['password'] !== WEBHOOK_PASSWORD){
-	exit('incorrect password');
-}
-
-$update = json_decode($update_json);
-if($update === null || $update === false){
-	exit('incorrect JSON input');
-}
-	
-$readableJson = json_encode($update, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT | JSON_NUMERIC_CHECK);
-$tracer->logEvent('[INCOMING MESSAGE]', __FILE__, __LINE__, PHP_EOL.$readableJson);
+require_once(__DIR__.'/Webhook.php');
 
 $botFactory = new TelegramBotFactory();
 $updateHandler = new UpdateHandler($botFactory);
-$updateHandler->handleUpdate($update);
+$webhook = new Webhook($updateHandler);
 
+$password = isset($_GET['password']) ? $_GET['password'] : null;
+$updateJSON = file_get_contents('php://input');
+assert($updateJSON !== false);
 
-if(defined('MESSAGE_STREAM_URL')){
-	try{
-		$testStream = new HTTPRequester();
-		$url = MESSAGE_STREAM_URL.'?password='.MESSAGE_STREAM_PASSWORD;
-		$testStream->sendJSONRequest($url, $update_json);
-	}
-	catch(Exception $ex){
-
-	}
-}
-
+$webhook->processUpdate($password, $updateJSON);
