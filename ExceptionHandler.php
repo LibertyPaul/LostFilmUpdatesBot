f82954ae82693aa7e1139c9a455a8bf5f2d68561
LibertyPaul <admin@libertypaul.ru>
2017-04-15 12:39:25 +0100

    Fix for Tracer namespace

--- ExceptionHandler.php
+++ ExceptionHandler.php
@@ -6,10 +6,12 @@ require_once(__DIR__.'/Tracer.php');
 function exception_handler($ex){
 	static $tracer;
 	if(isset($tracer) === false){
-		$tracer = new Tracer(__NAMESPACE__);
+		$tracer = new \Tracer(__NAMESPACE__);
 	}
 
 	$tracer->logException('[UNCAUGHT EXCEPTION]', __FILE__, __LINE__, $ex);
+
+	exit;
 }
 
 $res = set_exception_handler('ExceptionHandler\exception_handler');
