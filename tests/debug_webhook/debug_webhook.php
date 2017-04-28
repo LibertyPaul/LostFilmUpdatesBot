174578337d694ca447e66b0b9bd20d93730d2129
LibertyPaul <admin@libertypaul.ru>
2017-04-28 20:19:39 +0100

    Fixed debug_webhook

--- tests/debug_webhook/debug_webhook.php
+++ tests/debug_webhook/debug_webhook.php
@@ -1,26 +1,31 @@
 <?php
 require_once(__DIR__.'/../../ErrorHandler.php');
 require_once(__DIR__.'/../../ExceptionHandler.php');
-require_once(__DIR__.'/../config/Config.php');
-require_once(__DIR__.'/../BotPDO.php');
 
-require_once(__DIR__.'/../TelegramBotMockFactory.php');
+require_once(__DIR__.'/../../TelegramAPI.php');
 require_once(__DIR__.'/../../UpdateHandler.php');
 require_once(__DIR__.'/../../webhook/Webhook.php');
+require_once(__DIR__.'/../../BotPDO.php');
+require_once(__DIR__.'/../../HTTPRequesterFactory.php');
 
 require_once(__DIR__.'/input_debug_webhook.php');
-
-$dumpFile = tempnam('/tmp', 'debug_webhook');
-assert($dumpFile !== false);
-
-$botFactory = new TelegramBotMockFactory($dumpFile);
-$updateHandler = new UpdateHandler($botFactory);
-$webhook = new Webhook($updateHandler);
 
 $config = new Config(BotPDO::getInstance());
 $password = $config->getValue('Webhook', 'Password');
 
 $updateJSON = $update_json;
-assert(isset($updateJSON) && $updateJSON !== false);
+assert($updateJSON !== false);
 
+$HTTPRequesterFactory = new HTTPRequesterFactory();
+$HTTPRequester = $HTTPRequesterFactory->getInstance();
+
+$config = new Config(BotPDO::getInstance());
+$botToken = $config->getValue('TelegramAPI', 'token');
+assert($botToken !== null);
+
+$telegramAPI = new TelegramAPI($botToken, $HTTPRequester);
+
+$updateHandler = new UpdateHandler($telegramAPI);
+
+$webhook = new Webhook($updateHandler);
 $webhook->processUpdate($password, $updateJSON);
