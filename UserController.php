0893a2eb6dd05bb0b19ed9921eea44e77a0af14e
LibertyPaul <admin@libertypaul.ru>
2017-04-30 01:05:18 +0100

    Removed undefined conversationStorage usage

--- UserController.php
+++ UserController.php
@@ -57,7 +57,6 @@ class UserController{
 		if($this->user_id === null){
 			$this->user_id = $this->getUserIdByTelegramId($this->telegram_id);
 			if($this->user_id === null){// TODO: move error handling to another place
-				$conversationStorage->deleteConversation();
 				$this->tracer->logError('[ERROR]', __FILE__, __LINE__, 'Unknown Telegram Id: '.$this->telegram_id);
 				throw new UserIsNotRegistredException();
 			}
