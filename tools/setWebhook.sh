1b0a0f13162b959bdc50d697877ff37d67cba3d0
LibertyPaul <admin@libertypaul.ru>
2017-05-02 20:18:06 +0100

    setWebhook minor improvements

--- tools/setWebhook.sh
+++ tools/setWebhook.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-	echo "Usage: $0 <link_path>"
+if [ -z "$1" ] || [ -z "$2" ]; then
+	echo "Usage: $0 <API Name> [-f]"
 	exit 1
 fi
 
 readonly selfDir="$(dirname "$0")"
+readonly APIName="$1"
+readonly linkPath="$2"
+readonly force="$3"
+
 readonly getConfigValueScriptPath="$selfDir/getConfigValue.sh"
 
-readonly token="$("$getConfigValueScriptPath" 'TelegramAPI' 'token')"
+readonly token="$("$getConfigValueScriptPath" "$APIName" 'token')"
 if [ -z "$token" ]; then
-	echo '[TelegramAPI][token] is not found'
+	echo "[$APIName][token] is not found"
 	exit 1
 fi
 
@@ -32,8 +36,14 @@ fi
 
 echo "[DB] webhookPassword=[$webhookPassword]"
 
-readonly webhookPath="$selfDir/../webhook/webhook.php"
-readonly linkPath="$1"
+readonly APIDir="$selfDir/../$APIName"
+
+if ! [ -d "$APIDir" ]; then
+	echo "API '$APIName' does not exist under '$APIDir'"
+	exit 1
+fi
+
+readonly webhookPath="$APIDir/webhook.php"
 
 readonly linkDir="$(dirname "$linkPath")"
 if [ ! -d "$linkDir" ]; then
@@ -47,13 +57,22 @@ fi
 
 absWebhookPath=$(readlink -f "$webhookPath")
 if [ -z "$absWebhookPath" ]; then
-	echo "Bot webhook doesn't exist ($absWebhookPath)"
+	echo "Bot webhook doesn't exist ($webhookPath)($absWebhookPath)"
 	exit 1
 fi
 
 if [ -e "$linkPath" ]; then
-	echo "Link already exists. Please remove it first: $absLinkPath"
-	exit 1
+	if [ "$force" == "-f" ]; then
+		echo "Removing old link..."
+		rm -f "$linkPath"
+		if [ "$?" != "0" ]; then
+			echo "Error on 'rm -f $linkPath'. Aborting."
+			exit 1
+		fi
+	else
+		echo "Link already exists. Please remove it first: $absLinkPath or use -f option"
+		exit 1
+	fi
 fi
 
 absLinkPath=$(readlink -m "$linkPath")
