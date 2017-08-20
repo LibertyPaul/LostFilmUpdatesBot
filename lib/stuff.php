ccc19df176423f4c38684bed58cc0cd85e3d546e
LibertyPaul <admin@libertypaul.ru>
2017-08-20 03:09:03 +0100

    +Memcached factory

--- lib/stuff.php
+++ lib/stuff.php
@@ -10,9 +10,25 @@ function createMemcache(){
 		$res = $memcache->connect('localhost', 11211);
 		if($res === false){
 			unset($memcache);
-			throw new \RuntimeException('memcached connect error');
+			throw new \RuntimeException('memcache connection error');
 		}
 	}
 	return $memcache;
 }
 
+
+function createMemcached(){
+	static $memcached;
+
+	if(isset($memcached) === false){
+		$memcached = new \Memcached();
+		
+		$res = $memcached->addServer('localhost', 11211);
+		if($res === false){
+			unset($memcached);
+			throw new \RuntimeException('memcached connection error');
+		}
+	}
+
+	return $memcached;
+}
