dddf240ef9976a473a44bafe2a5c59cea19c8db3
LibertyPaul <admin@libertypaul.ru>
2017-09-03 00:38:47 +0100

    URL and Password will be taken from DB

--- tools/incomingMessagesToBot.sh
+++ tools/incomingMessagesToBot.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
 
-readonly URL=$1
-readonly password=$2
-readonly incomingMessages=$3
 
-if [ -z $URL ] || [ ! -r $incomingMessages ]; then
-	echo "Usage: $0 <URL> <Password> <Incoming Messages Trace Path>"
-	exit -1
+readonly incomingMessages=$1
+
+if [ -z "$incomingMessages" ]; then
+	echo "Usage: $0 <Incoming Messages Trace Path>"
+	exit 1
+fi
+
+if [ ! -r $incomingMessages ]; then
+	echo "Unable to access file $incomingMessages"
+	exit 1
+fi
+
+readonly selfPath=$(dirname $0)
+readonly URL=$("$selfPath/getConfigValue.sh" 'TelegramAPI' 'Webhook URL')
+readonly password=$("$selfPath/getConfigValue.sh" 'TelegramAPI' 'Webhook Password')
+
+if [ -z $URL ]; then
+	echo "ERROR: URL is not set. Aborting."
+	exit 1
+fi
+
+if [ -z $password ]; then
+	echo "WARNING: Password is not set"
+	read -p 'Continue? [Y/n]: ' yn
+	if [ -z $yn ]; then
+		yn='y'
+	fi
+
+	case $yn in
+		Yy) ;;
+		nN) exit 0;;
+		*) 	echo 'Unknown input. Aborting'
+			exit 1;;
+	esac
 fi
 
 current=''
