2aa3e3f119c548d67ba746c41bae915c5bb8ca36
LibertyPaul <admin@libertypaul.ru>
2017-09-10 14:18:47 +0100

    Added 'SET timezone' call on DB Connection init +minor improvements

--- lib/PDOInit.php
+++ lib/PDOInit.php
@@ -1,34 +1,34 @@
 <?php
 
 class PDOInit{
+	private static function getValue($src, $key, $default = null){
+		if(array_key_exists($key, $src) == false){
+			# func_num_args to ensure that $default was explicitly set in call
+			if(func_num_args() > 2){
+				return $default;
+			}
+
+			throw new \RuntimeException("$src was not found in ini file '$credentialsFile'");
+		}
+
+		return $src[$key];
+	}
+
 	private static function getCredentials($credentialsFile){
-		$values = parse_ini_file($credentialsFile);
-		if($values === false){
+		$iniFileData = parse_ini_file($credentialsFile, true);
+		if($iniFileData === false){
 			throw new \RuntimeException("parse_ini_file error '$credentialsFile'");
 		}
 
-		$result = array();
+		$MySQLConfig = self::getValue($iniFileData, 'mysql');
 
-		if(array_key_exists('database', $values)){
-			$result['database'] = $values['database'];
-		}
-		else{
-			throw new \RuntimeException("Database is not specified in '$credentialsFile'");
-		}
-
-		if(array_key_exists('user', $values)){
-			$result['user'] = $values['user'];
-		}
-		else{
-			throw new \RuntimeException("Username is not specified in '$credentialsFile'");
-		}
-
-		if(array_key_exists('password', $values)){
-			$result['password'] = $values['password'];
-		}
-		else{
-			throw new \RuntimeException("Password is not specified in '$credentialsFile'");
-		}
+		$result = array(
+			'host' => self::getValue($MySQLConfig, 'host', 'localhost'),
+			'database' => self::getValue($MySQLConfig, 'database'),
+			'charset' => self::getValue($MySQLConfig, 'charset', 'utf8mb4'),
+			'user' => self::getValue($MySQLConfig, 'user'),
+			'password' => self::getValue($MySQLConfig, 'password')
+		);
 
 		return $result;
 	}
@@ -36,12 +36,21 @@ class PDOInit{
 	public static function initialize($credentialsFile){
 		$credentials = self::getCredentials($credentialsFile);
 
-		$instance = new PDO(
-			'mysql:dbname='.$credentials['database'].';host=localhost;charset=utf8mb4',
+		$instance = new \PDO(
+			sprintf(
+				'mysql:dbname=%s;host=%s;charset=%s',
+				$credentials['database'],
+				$credentials['host'],
+				$credentials['charset']
+			),
 			$credentials['user'],
 			$credentials['password'],
 			array(
-				PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+				\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+				\PDO::MYSQL_ATTR_INIT_COMMAND => sprintf(
+					"SET time_zone = '%s'",
+					date_default_timezone_get()
+				)
 			)
 		);
 		
