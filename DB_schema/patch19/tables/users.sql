1c0c8e80450ce796da58785539ddfc39feeb1a17
LibertyPaul <admin@libertypaul.ru>
2017-09-22 21:56:15 +0100

    Minor patch fixes

--- DB_schema/patch19/tables/users.sql
+++ DB_schema/patch19/tables/users.sql
@@ -4,18 +4,40 @@ ALTER TABLE `users`
 		COLLATE ascii_bin
 		NOT NULL
 		DEFAULT 'TelegramAPI'
-		AFTER  `id`,
+		AFTER  `id`;
+
+ALTER TABLE `users`
 	CHANGE `API`
 		`API` ENUM('TelegramAPI', 'VKAPI')
 		CHARACTER SET ASCII
 		COLLATE ascii_bin
-		NOT NULL,
-	CHANGE `telegram_id`
-		`APIIdentifier` INT(10) UNSIGNED
-		NOT NULL,
-	DROP INDEX `telegram_id`,
-	ADD UNIQUE (`API`, `APIIdentifier`),
-	ADD INDEX (`APIIdentifier`),
+		NOT NULL;
+
+ALTER TABLE `users` 
+	ADD COLUMN `APIIdentifier`
+		INT(15) NOT NULL; 
+
+UPDATE `users`
+	SET `APIIdentifier` = `telegram_id`;
+
+ALTER TABLE `telegramUserData`
+	DROP FOREIGN KEY `telegramUserData_ibfk_1`;
+
+ALTER TABLE `users`
+	ADD UNIQUE (`API`, `APIIdentifier`);
+	    
+ALTER TABLE `users`
+	ADD INDEX (`APIIdentifier`);
+
+ALTER TABLE `telegramUserData`
+	ADD CONSTRAINT APIIdentifier_ibfk_1
+		FOREIGN KEY (`telegram_id`)
+		REFERENCES `users`(`APIIdentifier`);
+
+ALTER TABLE `users` 
+	DROP COLUMN `telegram_id`;
+
+ALTER TABLE `users`
 	CHANGE `mute`
 		`mute` ENUM('Y', 'N')
 		CHARACTER SET ASCII
