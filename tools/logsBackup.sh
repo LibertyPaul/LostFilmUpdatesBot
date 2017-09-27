c668bf461790123e948f9be6de818fbdabd3849d
LibertyPaul <admin@libertypaul.ru>
2017-09-27 19:07:05 +0100

    Logs backup by wildcard

--- tools/logsBackup.sh
+++ tools/logsBackup.sh
@@ -1,37 +1,92 @@
 #!/bin/bash
 
-readonly selfPath=$(dirname "$0")
-readonly logsDir="$selfPath/../logs"
+readonly selfDir="$(dirname "$0")"
+readonly logsDir="$selfDir/../logs"
 readonly logsBackupDir="$logsDir/../logsBackup"
+
+readonly coloredEchoPath="$selfDir/ColoredEcho.sh"
+source "$coloredEchoPath"
+if [ "$?" != "0" ]; then
+	echo "Unable to load [$coloredEchoPath]. Aborting."
+	exit 1
+fi
+
+searchMask="$1"
+if [ -z "$searchMask" ]; then
+	echo "SearchMask is not specified, using wildcard ... "
+	searchMask="*"
+fi
 
 
 if [ ! -d "$logsDir" ]; then
-	echo "$logsDir doesn't exist"
+	echo_red "$logsDir doesn't exist. Aborting."
 	exit 1
 fi
 
 if [ ! -d "$logsBackupDir" ]; then
 	if [ -f "$logsBackupDir" ]; then
-		echo "$logsBackupDir exists and is not a directory. Aboring."
+		echo_red "$logsBackupDir exists and is not a directory. Aboring."
 		exit 1
 	fi
 
 	mkdir "$logsBackupDir"
 	if [ "$?" != 0 ]; then
-		echo "'mkdir \"$logsBackupDir\"' error"
+		echo_red "'mkdir \"$logsBackupDir\"' error. Aborting."
 		exit 1
 	fi
 fi
 
-tmpDir=$(mktemp -d --suffix='_LFUB_LOGS' 2>&1)
-mv $logsDir/* $tmpDir
-
-readonly nameFormat="%y.%m.%d_%T.tar.gz"
-archiveName=$(date "+$nameFormat")
-
-export GZIP=-9
-tar cvzf "$logsBackupDir/$archiveName" --remove-files "$tmpDir"
-if [ "$?" != 0 ]; then
-	echo "Tar has failed. Old logs are preserved in $tmpDir"
+printf "Creating temporary directory ... "
+readonly tmpDir="$(mktemp -d --suffix='_LFUB_LOGS' 2>&1)"
+if [ "$?" == "0" ]; then
+	printf "$tmpDir "
+	echo_green "Success."
+else
+	echo_red "Fail. Aborting."
 	exit 1
 fi
+
+for tracePath in $(find "$logsDir" -name "$searchMask" -type f); do
+	traceName="$(basename "$tracePath")"
+	printf "Moving $tracePath to $tmpDir/$traceName ... "
+	mv "$tracePath" "$tmpDir/$traceName"
+	if [ "$?" == "0" ]; then
+		echo_green "Success."
+	else
+		echo_red "Fail."
+	fi
+done
+
+if [ $(ls "$tmpDir" | wc -l) == 0 ]; then
+	echo_yellow "No files matching pattern [$searchMask] were found."
+	
+	printf "Cleaning up ... "
+	rmdir "$tmpDir"
+	if [ "$?" == "0" ]; then
+		echo_green "Success."
+	else
+		echo_red "Fail."
+		exit 1
+	fi
+
+	exit 0
+fi
+
+printf "Archive name is ... "
+archiveName="$(date "+%y.%m.%d_%T.tar.gz")"
+if [ "$searchMask" != "*" ]; then
+	archiveName="[$searchMask].$archiveName"
+fi
+echo "$archiveName"
+
+echo "Compressing ... "
+export GZIP=-9
+tar cvzf "$logsBackupDir/$archiveName" --remove-files "$tmpDir"
+if [ "$?" == 0 ]; then
+	echo_green "Success."
+else
+	echo_red "Tar has failed. Old logs are preserved in $tmpDir."
+	exit 1
+fi
+
+
