e60f695ad170d0aedb1578487dd8b779123ad9d9
LibertyPaul <admin@libertypaul.ru>
2017-10-06 20:57:44 +0100

    Config now can work in 2 modes: 1-all values are loaded at startup; 2-values are locaded when requested;

--- lib/Config.php
+++ lib/Config.php
@@ -2,14 +2,18 @@
 
 require_once(__DIR__.'/Tracer/Tracer.php');
 
+abstract class ConfigFetchMode{
+	const ALL_AT_ONCE = 0;
+	const PER_REQUEST = 1;
+}
+
 class Config{
 	private $cachedValues; // array('section' => array('item' => 'value'))
 	private $getValueQuery;
 	private $tracer;
+	private $allCached = false;
 
-	public function __construct(PDO $pdo){
-		assert($pdo !== null);
-
+	public function __construct(\PDO $pdo, $mode = ConfigFetchMode::ALL_AT_ONCE){
 		$this->tracer = new \Tracer(__CLASS__);
 
 		$this->getValueQuery = $pdo->prepare('
@@ -20,6 +24,10 @@ class Config{
 		');
 
 		$this->cachedValues = array();
+
+		if($mode === ConfigFetchMode::ALL_AT_ONCE){
+			$this->loadAllValues($pdo);
+		}
 	}
 
 	private function cacheValue($section, $item, $value){
@@ -28,6 +36,35 @@ class Config{
 		}
 
 		$this->cachedValues[$section][$item] = $value;
+	}
+
+	private function loadAllValues(\PDO $pdo){
+		$getAllValuesQuery = $pdo->prepare('
+			SELECT `section`, `item`, `value`
+			FROM `config`
+		');
+
+		$this->tracer->logEvent(
+			'[CONFIG]', __FILE__, __LINE__,
+			'Loading all `config` values at once.'
+		);
+
+		$getAllValuesQuery->execute();
+
+		while(($res = $getAllValuesQuery->fetch()) !== false){
+			$this->cacheValue($res['section'], $res['item'], $res['value']);
+			$this->tracer->logDebug(
+				'[CONFIG]', __FILE__, __LINE__,
+				sprintf('[%s][%s][%s]', $res['section'], $res['item'], $res['value'])
+			);
+		}
+		
+		$this->tracer->logEvent(
+			'[CONFIG]', __FILE__, __LINE__,
+			'All values were loaded.'
+		);
+
+		$this->allCached = true;
 	}
 
 	public function getValue($section, $item, $defaultValue = null){
@@ -39,12 +76,28 @@ class Config{
 		if(array_key_exists($section, $this->cachedValues)){
 			if(array_key_exists($item, $this->cachedValues[$section])){
 				$value = $this->cachedValues[$section][$item];
-				$this->tracer->logDebug(
-					'[CONFIG GET]', __FILE__, __LINE__,
-					"Value=[$value] was found in cache"
-				);
-				return $value;
+
+				if($value !== null){
+					$this->tracer->logDebug(
+						'[CONFIG GET]', __FILE__, __LINE__,
+						"Value=[$value] was found in cache"
+					);
+
+					return $value;
+				}
+				else{
+					$this->tracer->logNotice(
+						'[CONFIG GET]', __FILE__, __LINE__,
+						"Absence of [$section][$item] is already cached"
+					);
+
+					return $defaultValue;
+				}	
 			}
+		}
+
+		if($this->allCached){
+			return $defaultValue;
 		}
 
 		$this->tracer->logDebug(
@@ -65,6 +118,9 @@ class Config{
 				'[CONFIG GET]', __FILE__, __LINE__,
 				"Requested value [$section][$item] does not exist"
 			);
+
+			$this->cacheValue($section, $item, null);
+
 			return $defaultValue;
 		}
 
