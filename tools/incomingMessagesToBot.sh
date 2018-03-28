94d0346d7b72ca9214abc5536ffbf80a7698dd8d
LibertyPaul <admin@libertypaul.ru>
2018-03-28 22:32:42 +0100

    Multi-threaded message flooding

--- tools/incomingMessagesToBot.sh
+++ tools/incomingMessagesToBot.sh
@@ -12,9 +12,9 @@ if [ ! -d "$incomingMessagesDir" ]; then
 	exit 1
 fi
 
-readonly selfDir=$(dirname $0)
-readonly address=$("$selfDir/getConfigValue.sh" 'TelegramAPI' 'Webhook URL')
-readonly password=$("$selfDir/getConfigValue.sh" 'TelegramAPI' 'Webhook Password')
+readonly selfDir="$(dirname $0)"
+readonly address="$("$selfDir/getConfigValue.sh" 'TelegramAPI' 'Webhook URL')"
+readonly password="$("$selfDir/getConfigValue.sh" 'TelegramAPI' 'Webhook Password')"
 
 
 if [ -z $address ]; then
@@ -39,4 +39,42 @@ else
 	URL="$address?password=$password"
 fi
 
+<<<<<<< HEAD
 find "$incomingMessagesDir" -type f | xargs -n 1 -P 32 "$selfDir/messageToBot.sh" "$URL"
+=======
+readonly URL="$address?password=$password"
+
+current=''
+i=0
+
+readonly messagesTmpDir=$(mktemp -d)
+readonly batchSize=8
+
+printf "Extracting messages to [$messagesTmpDir]... "
+
+cat "$incomingMessages" | while read line; do
+	if [[ "$line" =~ EVENT* ]]; then
+		if [ -n "$current" ]; then
+			i=$(($i+1))
+			messagePath="$messagesTmpDir/$i.txt"
+			echo "$current" > "$messagePath"
+			current=''
+		fi
+		continue
+	fi
+
+	current="$current$line";
+done;
+
+printf "Done. %d messages extracted.\n" $i
+
+echo "Sending all the messages... "
+date
+
+find "$messagesTmpDir" -type f | xargs -n 1 -P 32 "$selfDir/messageToBot.sh" $URL
+
+printf "Done."
+date
+
+rm -r "$messagesTmpDir"
+>>>>>>> 808d8f3... Multi-threaded message flooding
