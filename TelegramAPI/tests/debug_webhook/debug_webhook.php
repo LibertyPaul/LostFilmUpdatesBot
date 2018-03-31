a3d062deac9cf998c693a639460f0ec278d625f8
LibertyPaul <admin@libertypaul.ru>
2018-03-31 15:38:45 +0100

    New debug_webhook test data design

--- TelegramAPI/tests/debug_webhook/debug_webhook.php
+++ TelegramAPI/tests/debug_webhook/debug_webhook.php
@@ -14,13 +14,20 @@ require_once(__DIR__.'/../../UpdateHandler.php');
 require_once(__DIR__.'/../../Webhook.php');
 
 require_once(__DIR__.'/../../../lib/Tracer/Tracer.php');
-require_once(__DIR__.'/input_debug_webhook.php');
 
-$tracer = new \Tracer('DebugWebhook');
+$tracer = new \Tracer(__NAMESPACE__.'DebugWebhook');
 $tracer->logEvent('[DEBUG]', __FILE__, __LINE__, 'Debug Webhook was started');
 
-$updateJSON = $update_json;
-assert($updateJSON !== false);
+$updateJSON = file_get_contents(__DIR__.'/update.json');
+if($updateJSON === false){
+    $tracer->logError(
+        '[o]', __FILE__, __LINE__,
+        'update.json was not found'
+    );  
+
+    exit;
+}
+
 
 $config = new \Config(\BotPDO::getInstance());
 $password = $config->getValue('TelegramAPI', 'Webhook Password');
