15fa9d057050925a97fca91d7e16aa72f0b1dfac
Pavel Yazev <admin@libertypaul.ru>
2020-04-30 17:59:47 +0200

    Patch for permissions refresh

---
+++ DB_schema/privileges/permissions/03-parser.sql
@@ -0,0 +1,7 @@
+REVOKE ALL PRIVILEGES					ON `&&db_name`.*			FROM '&&parser_db_user'@'&&parser_db_host';
+
+GRANT LOCK TABLES						ON `&&db_name`.*			TO '&&parser_db_user'@'&&parser_db_host';
+GRANT SELECT							ON `&&db_name`.`config`		TO '&&parser_db_user'@'&&parser_db_host';
+GRANT SELECT							ON `&&db_name`.`APIs`		TO '&&parser_db_user'@'&&parser_db_host';
+GRANT SELECT, INSERT					ON `&&db_name`.`series`		TO '&&parser_db_user'@'&&parser_db_host';
+GRANT SELECT, INSERT, UPDATE, DELETE	ON `&&db_name`.`shows`		TO '&&parser_db_user'@'&&parser_db_host';
