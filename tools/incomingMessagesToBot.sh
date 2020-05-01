74c855f217e371b28ca80aae617f44e8deb9646f
Pavel Yazev <admin@libertypaul.ru>
2020-05-01 19:18:00 +0200

    Message sender progress & per-status counters

--- tools/incomingMessagesToBot.sh
+++ tools/incomingMessagesToBot.sh
@@ -39,4 +39,60 @@ else
 	URL="$address?password=$password"
 fi
 
-find "$incomingMessagesDir" -type f | sort -z | xargs -n 1 -P 1 "$selfDir/messageToBot.sh" "$URL"
+declare -a statuses=()
+declare -a counters=()
+
+function getStatusID(){
+	local _status="$1"
+
+	for id in "${!statuses[@]}"; do
+		if [ "$_status" == "${statuses[$id]}" ]; then
+			return "$id"
+		fi
+	done
+
+	id="${#statuses[@]}"
+
+	statuses=("${statuses[@]}" "$_status")
+	counters=("${counters[@]}" 0)
+	return "$id"
+}
+
+function incStatus(){
+	local _status="$1"
+
+	getStatusID "$_status"
+	local _statusID="$?"
+
+	counters[$_statusID]=$((${counters[$_statusID]} + 1))
+}
+
+function printCounters(){
+	for _id in "${!statuses[@]}"; do
+		printf "%d. %s: %d\n" "$_id" "${statuses[$_id]}" "${counters[$_id]}"
+	done
+}
+
+updateCount="$(find "$incomingMessagesDir" -type f | wc -l)"
+updatesSent=0
+
+function printProgress(){
+	local _updatesSent="$1"
+	local _updateCount="$2"
+	printf "Total messages sent: %d / %d\n" "$_updatesSent" "$_updateCount"
+}
+
+
+for update in $(find "$incomingMessagesDir" -type f | sort); do
+	res="$("$selfDir/messageToBot.sh" "$URL" "$update" 2>/dev/null)"
+
+	status="$(echo "$res" | tail -n 1)"
+	incStatus "$status"
+	updatesSent=$(($updatesSent + 1))
+
+	clear
+	printCounters
+	echo ""
+	printProgress $updatesSent $updateCount
+done
+
